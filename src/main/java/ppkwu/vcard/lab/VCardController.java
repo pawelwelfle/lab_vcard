cb0702b18ec21c537335f0aeaf4e8fe676d63ce8
Paweł Welfle <pawel.welfle@gmail.com>
2020-12-15 15:01:55 +0100

    add ezvcard maven dependency and tried do simply vcard

--- src/main/java/ppkwu/vcard/lab/VCardController.java
+++ src/main/java/ppkwu/vcard/lab/VCardController.java
@@ -1,5 +1,9 @@
 package ppkwu.vcard.lab;
 
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.VCardVersion;
+import ezvcard.property.StructuredName;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,8 +15,16 @@ public class VCardController {
 
     @GetMapping("/vcard")
     @ResponseBody
-    public ResponseEntity getVCardURL(@RequestParam("name") String name, @RequestParam("location") String location) {
+    public String getVCardURL(@RequestParam("name") String name, @RequestParam("location") String location) {
         String panoramaURL = "https://panoramafirm.pl/szukaj?k=" + name + "&l=" + location;
-        return null;
+        VCard vcard = new VCard();
+        StructuredName n = new StructuredName();
+        n.setFamily(name);
+        n.setGiven("Firma:");
+        vcard.setStructuredName(n);
+
+        String str = Ezvcard.write(vcard).version(VCardVersion.V4_0).go();
+        System.out.println(str);
+        return str;
     }
 }
