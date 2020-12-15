7004ade4df70726cffffdd7006cd55403f7aa63b
Paweł Welfle <pawel.welfle@gmail.com>
2020-12-15 14:42:51 +0100

    create and prepare method to get some vcards

--- src/main/java/ppkwu/vcard/lab/VCardController.java
+++ src/main/java/ppkwu/vcard/lab/VCardController.java
@@ -1,7 +1,18 @@
 package ppkwu.vcard.lab;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VCardController {
+
+    @GetMapping("/vcard")
+    @ResponseBody
+    public ResponseEntity getVCardURL(@RequestParam("name") String name, @RequestParam("location") String location) {
+        String panoramaURL = "https://panoramafirm.pl/szukaj?k=" + name + "&l=" + location;
+        return null;
+    }
 }
